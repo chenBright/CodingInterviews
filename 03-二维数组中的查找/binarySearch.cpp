5891783852bd72f15f5f09453a7b962874b58087
chenBright <1021774709@qq.com>
2018-11-16 11:22:37 +0800

    Feature: 03 修复矩阵行列颠倒的bug

--- 03-二维数组中的查找/binarySearch.cpp
+++ 03-二维数组中的查找/binarySearch.cpp
@@ -7,8 +7,8 @@ class Solution {
 public:
     bool Find(int target, vector<vector<int> > array) {
         bool isFind = false;
-        int cols = array.size();
-        int rows = array[0].size();
+        int rows = array.size();
+        int cols = array[0].size();
         isFind = Find(target, array, 0, rows - 1, 0, cols - 1);
 
         return isFind;
@@ -21,9 +21,9 @@ private:
 
         int i = lowRow + (highRow - lowRow) / 2;
         int j = lowCol + (highCol - lowCol) / 2;
-        if (array[i][j] == target) {
+        if (target == array[i][j]) {
             return true;
-        } else if (array[i][j] > target) {
+        } else if (target < array[i][j]) {
             return Find(target, array, lowRow, highRow, lowCol, j - 1) || Find(target, array, lowRow, i - 1, j, highCol);
         } else {
             return Find(target, array, lowRow, highRow, j + 1, highRow) || Find(target, array, i + 1, highRow, lowCol, j);
