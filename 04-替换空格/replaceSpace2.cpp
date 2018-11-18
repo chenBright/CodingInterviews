b239bc3e7354f57ffb1a34c142f45f388e199a2b
chenBright <1021774709@qq.com>
2018-11-18 11:59:17 +0800

    Feature: 04 文档

--- 04-替换空格/replaceSpace2.cpp
+++ 04-替换空格/replaceSpace2.cpp
@@ -12,9 +12,9 @@ public:
 
         // 从后向前遍历替换
         for (int i = length - 1, j = len - 1; i >= 0 && j>= 0; --i) {
-            if (str[i] != ' ') {
+            if (str[i] != ' ') { // 不是空格，直接复制
                 str[j--] = str[i];
-            } else {
+            } else { // 空格，移位
                 str[j--] = '0';
                 str[j--] = '2';
                 str[j--] = '%';
