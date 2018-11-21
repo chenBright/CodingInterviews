04b1b56dbb5a7d57cccf90e6b6fd8b68595e97fb
chenBright <1021774709@qq.com>
2018-11-21 19:08:50 +0800

    Feature: 07 stack2queue2

---
+++ 07-用两个栈实现队列/stack2queue2.cpp
@@ -0,0 +1,49 @@
+#include <stack>
+#include <limits.h>
+using namespace std;
+
+/**
+ * 将stack1作为输入栈，stack2作为输出栈。
+ * 调用push时，将结点push到stack1中；
+ * 调用pop时，在stack2中pop
+ */ 
+class Solution {
+public:
+    void push(int node) {
+        stack1.push(node);
+    }
+
+    int pop() {
+        // stack1和stack2都为空，则队列为空
+        if (stack1.empty() && stack2.empty()) {
+            return INT_MAX;
+        }
+
+        int queueHeadNode; // 队头结点
+        // stack1、stack2不全为空
+        if (stack2.empty()) {
+            // stack2为空，则stack1肯定不为空
+            while (!stack1.empty()) {
+                int temp = stack1.top();
+                // stack1长度为1，则此节点即为队头结点
+                // 否则，不是队头结点，将其push到stack2中
+                if (stack1.size() == 1) {
+                    queueHeadNode = temp;
+                } else {
+                    stack2.push(temp);
+                }
+                stack1.pop();
+           }
+        } else {
+            // 输出栈stack2不为空，则直接pop出队头结点
+            queueHeadNode = stack2.top();
+            stack2.pop();
+        }
+
+        return queueHeadNode;
+    }
+
+private:
+    stack<int> stack1;
+    stack<int> stack2;
+};
