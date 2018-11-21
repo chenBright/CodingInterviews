0bc7a04c400db6878379d77907fc89df864cf7a4
chenBright <1021774709@qq.com>
2018-11-21 23:45:21 +0800

    Feature: 07 文档

--- 07-用两个栈实现队列/stack2queue.cpp
+++ 07-用两个栈实现队列/stack2queue.cpp
@@ -2,6 +2,11 @@
 #include <limits.h>
 using namespace std;
 
+/**
+ * 将stack1作为存储区，stack2作为临时缓冲区。
+ * 入队时，将元素压入stack1。
+ * 出队时，将stack1逐个弹出并压入stack2中，将stack2的队头元素弹出作为出队元素，之后再将stack2剩下的元素逐个弹出并压入stack1。 
+ */
 class Solution {
 public:
     void push(int node) {
@@ -14,21 +19,21 @@ public:
             return -INT_MAX;
         }
 
-        int queueHeadNode; // 队头结点
+        int queueHeadNode; // 队头元素
         while (!stack1.empty()) {
             int temp = stack1.top();
-            // stack1长度为1，则此节点即为队头结点
-            // 否则，不是队头结点，将其push到stack2中
+            // stack1长度为1，则此元素即为队头结点
+            // 否则，不是队头元素，将其压入到stack2中
             if (stack1.size() == 1) {
                 queueHeadNode = temp;
             } else {
                 stack2.push(temp);
             }
-            // 在stack1中删除该结点
+            // 在stack1中弹出该元素
             stack1.pop();
         }
 
-        // 将结点重新 push 到 stack1 中
+        // 将结点重新压入到stack1中
         while (!stack2.empty()) {
             int temp = stack2.top();
             stack2.pop();
--- 07-用两个栈实现队列/stack2queue2.cpp
+++ 07-用两个栈实现队列/stack2queue2.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 /**
  * 将stack1作为输入栈，stack2作为输出栈。
- * 调用push时，将结点push到stack1中；
- * 调用pop时，在stack2中pop
+ * 调用push时，将元素压入到stack1中；
+ * 调用pop时，在stack2中弹出
  */ 
 class Solution {
 public:
@@ -19,14 +19,14 @@ public:
             return INT_MAX;
         }
 
-        int queueHeadNode; // 队头结点
+        int queueHeadNode; // 队头元素
         // stack1、stack2不全为空
         if (stack2.empty()) {
             // stack2为空，则stack1肯定不为空
             while (!stack1.empty()) {
                 int temp = stack1.top();
-                // stack1长度为1，则此节点即为队头结点
-                // 否则，不是队头结点，将其push到stack2中
+                // stack1长度为1，则此元素即为队头结点
+                // 否则，不是队头结点，将其压入到stack2中
                 if (stack1.size() == 1) {
                     queueHeadNode = temp;
                 } else {
@@ -35,7 +35,7 @@ public:
                 stack1.pop();
            }
         } else {
-            // 输出栈stack2不为空，则直接pop出队头结点
+            // 输出栈stack2不为空，则直接弹出队头结点
             queueHeadNode = stack2.top();
             stack2.pop();
         }
