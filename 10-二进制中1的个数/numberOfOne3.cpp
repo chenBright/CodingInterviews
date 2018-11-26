b2be9d97aaaa2f3151fa3c79a6f78f7b873d7c93
chenBright <1021774709@qq.com>
2018-11-26 10:51:41 +0800

    Feature: 10 numberOfOne3

---
+++ 10-二进制中1的个数/numberOfOne3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+     int  NumberOf1(int n) {
+         int count = 0;
+         unsigned int flag = 1;
+
+         while (flag) {
+             if (n & flag) {
+                 ++count;
+             }
+             flag <<= 1;
+         }
+
+         return count;
+     }
+};
