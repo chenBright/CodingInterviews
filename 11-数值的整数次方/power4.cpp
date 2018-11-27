24f3dc2fe23ddc5a7c1f41151547dd9819ee5860
chenBright <1021774709@qq.com>
2018-11-27 21:11:56 +0800

    Feature: 11 power4

---
+++ 11-数值的整数次方/power4.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    double Power(double base, int exponent) {
+        if (base == 0.0) {
+            return 0.0;
+        }
+
+        if (base == 1.0 || exponent == 0) {
+            return 1.0;
+        }
+
+        bool isNegative = false;
+        if (exponent < 0) {
+            isNegative = true;
+            exponent = -exponent;
+        } 
+
+        double result = 1.0;
+        double temp = base;
+        while (exponent != 0) {
+            // 奇数
+            if ((exponent & 1) == 1) {
+                result *= base;
+            }
+            temp *= temp;
+            exponent >>= 1;
+        }
+
+        if (isNegative) {
+            result = 1 / result;
+        }
+
+        return result;
+    }
+};
