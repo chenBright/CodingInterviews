406f5867eeead256d103665c6ac46961e9f4a885
chenBright <1021774709@qq.com>
2018-12-27 17:05:34 +0800

    Feature: 25-二叉树中和为某一值的路径

---
+++ 25-二叉树中和为某一值的路径/FindPath.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+
+class Solution {
+public:
+    vector<vector<int> > FindPath(TreeNode* root,int expectNumber) {
+        v.clear();
+        if (root == NULL || expectNumber == 0) {
+            return v;
+        }
+
+        vector<int> nodes;
+        FindPath(root, expectNumber, nodes);
+        return v;
+    }
+
+private:
+    vector<vector<int> > v;
+    void FindPath(TreeNode *node, int expectNumber, vector<int> nodes) {
+        // 结点为叶子结点，从根结点到该结点的和等于expectNumber，符合要求
+        if (node->val == expectNumber || (node->left == NULL && node->right == NULL)) { 
+            nodes.push_back(node->val);
+            v.push_back(nodes);
+        } else if (node->val < expectNumber) { // 不是叶子结点且和小于expectNumber，继续往下递归遍历
+            nodes.push_back(node->val);
+            expectNumber -= node->val; // 减
+            if (node->left !=  NULL) {
+                FindPath(node->left, expectNumber, nodes);
+            }
+            if (node->right != NULL) {
+                FindPath(node->right, expectNumber, nodes);
+            }
+        }
+
+    }
+};
--- README.md
+++ README.md
@@ -33,3 +33,4 @@
 - [22-栈的压入弹出序列](https://www.nowcoder.com/practice/d77d11405cc7470d82554cb392585106?tpId=13&tqId=11174&tPage=1&rp=1&ru=%2Fta%2Fcoding-interviews&qru=%2Fta%2Fcoding-interviews%2Fquestion-ranking)
 - [23-从上往下打印二叉树](https://www.nowcoder.com/practice/7fe2212963db4790b57431d9ed259701?tpId=13&tqId=11175&tPage=2&rp=1&ru=%2Fta%2Fcoding-interviews&qru=%2Fta%2Fcoding-interviews%2Fquestion-ranking)
 - [24-二叉搜索树的后序遍历序列](https://www.nowcoder.com/practice/a861533d45854474ac791d90e447bafd?tpId=13&tqId=11176&tPage=2&rp=1&ru=%2Fta%2Fcoding-interviews&qru=%2Fta%2Fcoding-interviews%2Fquestion-ranking)
+- [25-二叉树中和为某一值的路径](https://www.nowcoder.com/practice/b736e784e3e34731af99065031301bca?tpId=13&tqId=11177&tPage=2&rp=1&ru=%2Fta%2Fcoding-interviews&qru=%2Fta%2Fcoding-interviews%2Fquestion-ranking)
